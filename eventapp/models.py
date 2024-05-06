93610710baa7093e3b4aba43e267db34e1f3300d
serhiikravets <sergeykravets41@gmail.com>
2024-05-07 01:36:10 +0300

    make date timezone aware

--- eventapp/models.py
+++ eventapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 from users.models import User
 
 
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     location = models.CharField(max_length=100)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     attendees = models.ManyToManyField(User, related_name='attending')
